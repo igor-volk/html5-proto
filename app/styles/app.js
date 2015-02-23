ce1de1126fc8f0be21d65cfc4662c8930a882310
Igor Volk <igor.volk@bskyb.com>
2015-02-23 15:08:21 +0000

    Started. Nav font is distorted

---
+++ app/styles/app.js
@@ -0,0 +1,11 @@
+var navData = ["My Library", "My Account", "Help", "About sky Store"];
+
+// function buildnav() 
+// {
+// 	var nav = document.getElementById("nav");
+// 	var ctx = nav.getContext("2d");
+// 	ctx.font = "25px sky-regular";
+// 	ctx.fillText("Hello", 0, 75)
+// }
+
+//buildNav();
